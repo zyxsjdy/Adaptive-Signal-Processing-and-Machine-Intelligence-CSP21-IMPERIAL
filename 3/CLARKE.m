fa4a6d71fe8ae1c7ddddd696594711546183d900
TimZ <57795170+zyxsjdy@users.noreply.github.com>
2024-07-18 21:09:14 -0230

    Delete 3 directory

--- 3/CLARKE.m
+++
@@ -1,6 +0,0 @@
-function [V3_CT] = CLARKE(V3)
-Clarke_Matrix = sqrt(2/3) * [1/sqrt(2)  1/sqrt(2)  1/sqrt(2);   % generate the CLARKE matrix
-                             1          -1/2       -1/2; 
-                             0          sqrt(3)/2  -sqrt(3)/2];
-V3_CT = Clarke_Matrix * V3;  % apply the CLARKE transform
-end
